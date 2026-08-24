77f84d040281b6226b73bd3b030b69899bf9b700
Toby Harradine <tobyh@canva.com>
2026-08-24 16:57:07 +1000

    fix issues with linting in CI

--- bin/lint.sh
+++ bin/lint.sh
@@ -6,5 +6,5 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 repository_root="$(dirname "$script_dir")"
 cd "$repository_root"
 
-rg --files -0 -g '*.sh' -g '*.bash' -g '*.zsh' |
+git ls-files -z -- '*.sh' '*.bash' '*.zsh' |
   xargs -0 "$repository_root/bin/shellcheck"
